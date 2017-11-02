f3ce1bfcf48ef8205093a48b13b098e334f2c9b7
Tom Croasdale <tecroasdale1@sheffield.ac.uk>
2017-11-02 22:02:53 +0000

    Back to where I started this morning

---
+++ GithubGO2017/Assets/Scripts/CameraControl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour {
+
+	public Transform target;
+	public Vector3 offset = new Vector3(0, 4, -4);
+	public float moveSpeed = 4.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 tPos = target.position;
+		transform.position = Vector3.Lerp(transform.position, tPos + offset, Time.deltaTime *moveSpeed);
+		transform.LookAt(target);
+	}
+}
---
+++ GithubGO2017/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour {
+
+	public float moveSpeed = 4.0f;
+	public float rotSpeed = 4.0f;
+
+	public CharacterController c_Control;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float in_h = Input.GetAxis("Horizontal");
+		float in_v = Input.GetAxis("Vertical");
+
+		Quaternion prevRot = transform.rotation;
+		Vector3 dir = new Vector3(in_h, 0, in_v);
+		Quaternion targRot ;
+		if(dir != Vector3.zero){
+			targRot = Quaternion.LookRotation(dir, Vector3.up);
+		}else{
+			targRot = prevRot;
+		}
+
+		transform.rotation = Quaternion.Slerp(prevRot, targRot, rotSpeed * Time.deltaTime);
+
+		if(!c_Control.isGrounded){ dir.y = Physics.gravity.y * Time.deltaTime; }
+		c_Control.Move(new Vector3(dir.x * moveSpeed, dir.y, dir.z * moveSpeed));
+	}
+}

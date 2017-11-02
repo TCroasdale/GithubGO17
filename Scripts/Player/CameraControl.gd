ee717d40090ea6227bf566e160f24f2f7ab984a4
Tom Croasdale <tecroasdale1@sheffield.ac.uk>
2017-11-02 00:33:42 +0000

    player can be moved around

---
+++ Scripts/Player/CameraControl.gd
@@ -0,0 +1,8 @@
+extends Spatial
+
+var followSpeed = 2.0f
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	set_process(true)
--- Scripts/Player/PlayerControl.gd
+++ Scripts/Player/PlayerControl.gd
@@ -5,24 +5,39 @@ extends Spatial
 # var a = 2
 # var b = "textvar"
 
+var moveDir = Vector3(0,0,0)
+var targetDir = Vector3(0,0,0)
+
 func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
 	set_process(true)
+	set_fixed_process(true)
 
 func _process(delta):
-	var moveDir = get_input_dir()
-	get_node("Character").translate(Vector3(moveDir.x, 0, moveDir.y) * delta)
+	moveDir = get_input_dir()
+	
+func _fixed_process(delta):
+	doMove(moveDir, delta)
+	doRot(moveDir, delta)
 	
 	
-	
+func doMove(moveDir, delta):
+		#var node = get_node("Character")
+		move(moveDir * delta)
+
+func doRot(lookDir, delta):
+	var node = get_node("Mesh")
+	var actualDir = targetDir.linear_interpolate(lookDir, delta)
+	node.look_at(node.get_transform().origin - actualDir, Vector3(0, 1, 0))
+	targetDir = actualDir
 	
 func get_input_dir():
-	var dir = Vector2(0, 0)
+	var dir = Vector3(0, 0, 0)
 	if(Input.is_action_pressed("move_north")):
-		dir.y += 1
+		dir.z += 1
 	if(Input.is_action_pressed("move_south")):
-		dir.y -= 1
+		dir.z -= 1
 	if(Input.is_action_pressed("move_east")):
 		dir.x += 1
 	if(Input.is_action_pressed("move_west")):

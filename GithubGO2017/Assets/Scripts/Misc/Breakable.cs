3d6461f6ca47fee6a8ffc56c86f2f1c1896118a2
Tom Croasdale <tecroasdale1@sheffield.ac.uk>
2017-11-08 01:18:23 +0000

    added all files

---
+++ GithubGO2017/Assets/Scripts/Misc/Breakable.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Breakable : MonoBehaviour, iSuckable {
+
+	public GameObject cleanMesh;
+	public GameObject brokenMesh;
+
+	public float Health = 100.0f;
+
+	public Collider cleanCollider;
+	public Collider brokenCollider;
+
+	public shakeable shaker;
+
+	// Use this for initialization
+	void Start () {
+		reset();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void onSuck(Vector3 o, float p){
+		damage(p);
+		shaker.onSuck(o, p);
+	}
+
+	public void onBlow(Vector3 o, float p){
+		damage(p);
+		shaker.onSuck(o, p);
+	}
+
+	void damage(float amt){
+		Health -= amt;
+		if(Health <= 0.0f){
+			cleanMesh.SetActive(false);
+			brokenMesh.SetActive(true);
+			if(cleanCollider != null) cleanCollider.isTrigger = true;
+			if(brokenCollider != null) brokenCollider.isTrigger = false;
+			if(shaker != null) shaker.objToShake = brokenMesh.transform;
+		}
+	}
+
+	public void reset(){
+		cleanMesh.SetActive(true);
+		brokenMesh.SetActive(false);
+		if(cleanCollider != null) cleanCollider.isTrigger = false;
+		if(brokenCollider != null) brokenCollider.isTrigger = true;
+		if(shaker != null) shaker.objToShake = cleanMesh.transform;
+	
+	}
+}
